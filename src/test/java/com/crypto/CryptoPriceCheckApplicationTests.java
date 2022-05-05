f4a3bfcfdef6592231dc720bcf79be3db2256191
Sujit Dhali <sujit.dhali@gmail.com>
2022-05-05 14:09:24 +0530

    Update CryptoPriceCheckApplicationTests.java
    Test added.

--- src/test/java/com/crypto/CryptoPriceCheckApplicationTests.java
+++ src/test/java/com/crypto/CryptoPriceCheckApplicationTests.java
@@ -1,14 +1,69 @@
 package com.crypto;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-@SpringBootTest
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class CryptoPriceCheckApplicationTests {
 	
-	@Test
-	void contextLoads(){
-		
-	}
+	@LocalServerPort
+    int randomServerPort;
+    
+    @Test
+    public void testGetCrytoPriceRequestScreen() throws URISyntaxException 
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        
+        final String baseUrl = "http://localhost:"+randomServerPort+"/";
+        URI uri = new URI(baseUrl);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE).contains("text/html"));
+    }
+    
+    @Test
+    public void testGetCrytoPriceDetails_withoutIp() throws URISyntaxException 
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        
+        final String baseUrl = "http://localhost:"+randomServerPort+"/getcryptoprice?crypto=Bitcoin&ipaddress=";
+        URI uri = new URI(baseUrl);
+        
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE).contains("text/html"));
+    }
+    
+    @Test
+    public void testGetCrytoPriceDetails_withIp() throws URISyntaxException 
+    {
+        RestTemplate restTemplate = new RestTemplate();
+        
+        final String baseUrl = "http://localhost:"+randomServerPort+"/getcryptoprice?crypto=Bitcoin&ipaddress=104.166.185.255";
+        URI uri = new URI(baseUrl);
+
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+        
+        //Verify request succeed
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals(true, result.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE).contains("text/html"));
+    }
 
 }
